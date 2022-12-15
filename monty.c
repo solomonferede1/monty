821cc1063418c05dcaa9513f1ceb3be768b97b0b
solomon1227 <solomonf1227@gmail.com>
2022-12-15 21:46:53 +0300

    mainfunction

---
+++ monty.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * main - Entry Point
+ * @argc: Number of command line arguments.
+ * @argv: An array containing the arguments.
+ * Return: Always Zero.
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 2 || argc > 2)
+		err(1);
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+/**
+ * create_node - Creates and populates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
