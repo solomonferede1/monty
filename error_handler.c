823450746c1acdc0b6499adb6517bc98a19716cb
solomon1227 <solomonf1227@gmail.com>
2022-12-16 08:14:28 +0300

    Handle the error

--- error_handler.c
+++ error_handler.c
@@ -22,22 +22,22 @@ void err(int error_code, ...)
 	switch (error_code)
 	{
 		case 1:
-			printf("USAGE: monty file\n");
+			fprintf(stderr, "USAGE: monty file\n");
 			break;
 		case 2:
-			printf("Error: Can't open file %s\n",
+			fprintf(stderr, "Error: Can't open file %s\n",
 				va_arg(ag, char *));
 			break;
 		case 3:
 			l_num = va_arg(ag, int);
 			op = va_arg(ag, char *);
-			printf("L%d: unknown instruction %s\n", l_num, op);
+			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
 			break;
 		case 4:
-			printf("Error: malloc failed\n");
+			fprintf(stderr, "Error: malloc failed\n");
 			break;
 		case 5:
-			printf("L%d: usage: push integer\n", va_arg(ag, int));
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
 			break;
 		default:
 			break;
@@ -64,20 +64,20 @@ void more_err(int error_code, ...)
 	switch (error_code)
 	{
 		case 6:
-			printf("L%d: can't pint, stack empty\n",
+			fprintf(stderr, "L%d: can't pint, stack empty\n",
 				va_arg(ag, int));
 			break;
 		case 7:
-			printf("L%d: can't pop an empty stack\n",
+			fprintf(stderr, "L%d: can't pop an empty stack\n",
 				va_arg(ag, int));
 			break;
 		case 8:
 			l_num = va_arg(ag, unsigned int);
 			op = va_arg(ag, char *);
-			printf("L%d: can't %s, stack too short\n", l_num, op);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, op);
 			break;
 		case 9:
-			printf("L%d: division by zero\n",
+			fprintf(stderr, "L%d: division by zero\n",
 				va_arg(ag, unsigned int));
 			break;
 		default:
@@ -103,10 +103,10 @@ void string_err(int error_code, ...)
 	switch (error_code)
 	{
 		case 10:
-			printf("L%d: can't pchar, value out of range\n", l_num);
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
 			break;
 		case 11:
-			printf("L%d: can't pchar, stack empty\n", l_num);
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
 			break;
 		default:
 			break;
